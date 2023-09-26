24b832d3375b3797329b102d0a982b29018a2708
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2023-09-26 18:55:50 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from "./assets/logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={logo} className="logo" alt="App logo" />
+      </div>
+      <h1 className="text-iron">Iron Travels</h1>
+      <h3 className="text-iron">Tailored Travel Plans for Ironhackers</h3>
+
+      {/* RENDER YOUR LIST COMPONENT HERE */}
+      
+    </>
+  );
+}
+
+export default App;
